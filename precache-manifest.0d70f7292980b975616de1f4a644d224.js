500dcfd9b77fa5fb8f95058ba552908187ec2345
Ankrishx20 <ankrishkandpal1996@gmail.com>
2019-06-25 21:59:18 +0530

    Updates

---
+++ precache-manifest.0d70f7292980b975616de1f4a644d224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a63a841c99f3ecf0c5b1a6c98b9abce",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "83e6030101be34e43ee3",
+    "url": "/todoapp/static/css/main.0be1afe0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/todoapp/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "83e6030101be34e43ee3",
+    "url": "/todoapp/static/js/main.b858c871.chunk.js"
+  },
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  }
+]);
